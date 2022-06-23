b099fa55600442a43a236613fba588c30a1f30d2
PrincessKhay <102265555+PrincessKhay@users.noreply.github.com>
2022-06-23 21:49:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = document.getElementById("name");
+const height = document.getElementById("height");
+const country = document.getElementById("country");
+
+name.innerHTML = "Olusegun Princess Kemi";
+height.innerHTML = 1.82 + "m";
+country.innerHTML ="Nigeria";
